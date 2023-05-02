ef6b05f4f38592d7096450317d2b5bbb74f1b571
Michel <49735520+michelcub@users.noreply.github.com>
2023-05-02 19:38:21 +0000

    add new feature

--- app.js
+++ app.js
@@ -4,18 +4,44 @@ const noun = ["jogger", "racoon"];
 
 const btnGenerate = document.querySelector("#btn");
 const listDomain = document.querySelector("#list");
-let select = document.querySelector("#select").value;
+const select = document.querySelector("#select");
 const reset = document.querySelector("#btnReset");
+const input = document.querySelector("#inputName");
 
 const generationDomain = (pronoun, adj, noun, option) => {
   const pronounDomain = pronoun[Math.floor(Math.random() * 2 + 0)];
   const adjDomain = adj[Math.floor(Math.random() * 2 + 0)];
   const nounDomain = noun[Math.floor(Math.random() * 2 + 0)];
-  return `<li class ="list-group-item"> ${pronounDomain}${adjDomain}${nounDomain}${option} </li> `;
+  return (listDomain.innerHTML += `<li class ="list-group-item"> ${pronounDomain}${adjDomain}${nounDomain}${option} </li> `);
 };
 
+const generationDomainCustom = (input) => {
+  const stringInput = input.toString();
+  console.log("stringInput: ", stringInput);
+  const extencion = stringInput.slice(
+    stringInput.length - 2,
+    stringInput.length
+  );
+  console.log("extencion: ", extencion);
+  const nameDomain = stringInput.slice(0, stringInput.length - 2);
+  console.log("nameDomain: ", nameDomain);
+  let domainCustom = "";
+
+  if (stringInput.length >= 4) {
+    domainCustom = `${nameDomain}.${extencion}`;
+    return (listDomain.innerHTML += `<li class ="list-group-item">${domainCustom}</li>`);
+  } else {
+    alert("El valor de entrada no es valido");
+  }
+};
+
+const switchGenerationDomain = (option) => {
+  return option != "auto"
+    ? generationDomain(pronoun, adj, noun, select.value)
+    : generationDomainCustom(input.value);
+};
 btnGenerate.addEventListener("click", () => {
-  listDomain.innerHTML += generationDomain(pronoun, adj, noun, select);
+  switchGenerationDomain(select.value);
 });
 
 reset.addEventListener("click", () => {
